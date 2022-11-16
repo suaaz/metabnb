601c4da0afd0ebebf529608c517f620638ad87a0
suaaz <suaazini@gmail.com>
2022-11-15 18:05:09 -1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Navbar from './Navbar';
+import Body from './Body';
+import Place from './Place';
+import Footer from './Footer';
+import { Route ,Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        < Navbar />
+        <Routes>
+          <Route path='/' element={<Body />} />
+          <Route path='/place_to_stay' element={<Place />} />
+        </Routes>
+        < Footer />
+      </div>
+    </>
+
   );
 }
 
